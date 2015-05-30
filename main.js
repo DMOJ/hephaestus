b748d008c430e5a202f51fc08e1ce836a17c479c
Tudor <tbrindus@gmail.com>
2015-05-30 16:44:20 -0400

    ipc

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ require('crash-reporter').start();
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     if (process.platform != 'darwin')
         app.quit();
 });
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
     // Create the browser window.
     mainWindow = new BrowserWindow({width: 800, height: 600});
 
@@ -25,8 +25,16 @@ app.on('ready', function() {
     // and load the index.html of the app.
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
+
+    mainWindow.webContents.on('did-finish-load', function () {
+        mainWindow.webContents.send('problem-list', [
+            'dmpg15b1', 'dmpg15b2', 'dmpg15b3', 'dmpg15b4', 'dmpg15b5', 'dmpg15b6',
+            'dmpg15s1', 'dmpg15s2', 'dmpg15s3', 'dmpg15s4', 'dmpg15s5', 'dmpg15s6',
+            'dmpg15g1', 'dmpg15g2', 'dmpg15g3', 'dmpg15g4', 'dmpg15g5', 'dmpg15g6']);
+    });
+
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
